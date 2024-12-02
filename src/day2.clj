b435f7c3becd61e9a3860123fefc1f4fab2d8b43
Ben Sless <ben.sless@gmail.com>
2024-12-02 21:19:41 +0200

    Remove noise

--- src/day2.clj
+++ src/day2.clj
@@ -4,14 +4,6 @@
    [clojure.string :as str]
    [clojure.test :refer [set-test is]]))
 
-(def sample
-  "7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9")
-
 (defn safe-delta [l1 l2]
   (let [delta (abs (- l1 l2))]
     (and (<= 1 delta)
@@ -26,13 +18,6 @@
 (defn safe? [levels]
   (or (all-increasing? levels)
       (all-decreasing? levels)))
-
-;; 7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
-;; 1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
-;; 9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
-;; 1 3 2 4 5: Unsafe because 1 3 is increasing but 3 2 is decreasing.
-;; 8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
-;; 1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
 
 (set-test
  safe?
